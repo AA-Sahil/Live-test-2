3805095f32779b1f03556b4d98761a85d33e7370
AA-Sahil <141584723+AA-Sahil@users.noreply.github.com>
2024-09-19 22:49:43 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 // // 1. Write a function reverseString(str) that takes a string and returns it reversed. Ex: reverseString("hello"); // Output: "olleh"
 
 
-// function reverseString(str) {
-//     revStr= str.split('').reverse()
-//     return revStr.join('')
-// }
+function reverseString(str) {
+    revStr= str.split('').reverse()
+    return revStr.join('')
+}
 
-// console.log(reverseString("hello"))
+console.log(reverseString("hello"))
 
 
 
 // // 2. Write a function sumArray(arr) that takes an array of numbers and returns the sum of all the numbers. Ex: sumArray([1, 2, 3, 4]); // Output: 10
 
 
-// function sumArray(arr) {
-//     let sum = 0;
+function sumArray(arr) {
+    let sum = 0;
 
-//     for (let i = 0; i < arr.length; i++) {
-//         sum += arr[i]
-//     }
-//     return sum;
-// }
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum;
+}
 
-// let arr= [10, 20, 30, 40];
-// console.log(sumArray(arr))
+let arr= [10, 20, 30, 40];
+console.log(sumArray(arr))
 
 
 
 // // 3. Write a function removeDuplicates(arr) that takes an array and returns a new array with only unique values (duplicates removed). Ex: removeDuplicates([5, 5, 5, 6, 7]); // Output: [5, 6, 7]
 
 
-// function removeDuplicates(arr) {
-//     return[...new Set(arr)];
-// }
+function removeDuplicates(arr) {
+    return[...new Set(arr)];
+}
 
-// const arr = [1,1,2,66,66,22,22,2,2,66,22,0,0,0,1,1]
+const arr = [1,1,2,66,66,22,22,2,2,66,22,0,0,0,1,1]
 
-// console.log(removeDuplicates(arr))
+console.log(removeDuplicates(arr))
 
 
 
